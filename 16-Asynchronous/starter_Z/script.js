012990b1e7de95fa6c81e72e916bbd3360f2513d
zinaida <zinaida.j84@gmail.com>
2023-01-12 12:35:38 +0100

    promise chaining finished

--- 16-Asynchronous/starter_Z/script.js
+++ 16-Asynchronous/starter_Z/script.js
@@ -1,5 +1,9 @@
 'use strict';
 
+// const { data } = require('autoprefixer');
+
+// const { data } = require('autoprefixer');
+
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
@@ -32,6 +36,78 @@ const getCountryData = country => {
   });
 };
 
-getCountryData('Portugal');
-getCountryData('usa');
-getCountryData('austria');
+// getCountryData('Portugal');
+///////////////////////////////////////
+
+const renderCountry = (data, className = '') => {
+  const html = `
+    <article class="country ${className}">
+    <img class="country__img" src="${data.flags.png}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name.common}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        +data.population / 1000000
+      ).toFixed(1)} people</p>
+      <p class="country__row"><span>🗣️</span>${Object.values(
+        data.languages
+      )}</p>
+      <p class="country__row"><span>💰</span>${Object.keys(data.currencies)}</p>
+    </div>
+  </article>`;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryAndNeighbour = country => {
+  //AJAX Call country 1
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    // render first country
+    renderCountry(data);
+
+    // get neighbout country 2
+    const neighbour = data.borders?.[0];
+
+    //AJAX Call country 2
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour}`);
+    request2.send();
+
+    request2.addEventListener('load', function () {
+      const [data2] = JSON.parse(this.responseText);
+      console.log(data2);
+
+      // render first country
+      renderCountry(data2, 'neighbour');
+    });
+  });
+};
+
+// getCountryAndNeighbour('Bosnia');
+
+// //AJAX Call country 1
+// const request = new XMLHttpRequest();
+// request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+// request.send();
+
+const getCountryData2 = function (country) {
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then(response => response.json())
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbour = data[0]?.borders[0];
+      console.log(neighbour);
+      return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+    })
+    .then(response => response.json())
+    .then(data => renderCountry(data[0], 'neighbour'));
+};
+
+getCountryData2('bosnia');
