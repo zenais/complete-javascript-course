4313859d46dd553f6aa1b3c3859a8439d3402da3
zinaida <zinaida.j84@gmail.com>
2023-01-13 15:22:59 +0100

    second challenge finished

--- 16-Asynchronous/starter_Z/script.js
+++ 16-Asynchronous/starter_Z/script.js
@@ -60,11 +60,20 @@ const getCountryData = function (country) {
     });
 };
 
-const whereAmI = function (lat, lng) {
-  fetch(
-    `https://geocode.xyz/${lat},${lng}?geoit=json&auth=369140244988012877940x7595`
-  )
-    .then(response => response.json())
+const whereAmI = function () {
+  getPosition()
+    .then(pos => {
+      console.log(pos);
+      const { latitude: lat, longitude: lng } = pos.coords;
+      return fetch(
+        `https://geocode.xyz/${lat},${lng}?geoit=json&auth=369140244988012877940x7595`
+      );
+    })
+
+    .then(response => {
+      if (!response.ok) throw new Error('Problem with geocoding');
+      return response.json();
+    })
     .then(data => {
       getCountryData(data.country);
       console.log(`You are in ${data.city}, ${data.country}`);
@@ -74,9 +83,121 @@ const whereAmI = function (lat, lng) {
     );
 };
 
-const myLat = 48.210033;
-const myLong = 16.363449;
+const getPosition = function () {
+  return new Promise(function (resolve, reject) {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+};
 
 btn.addEventListener('click', function () {
-  whereAmI(myLat, myLong);
+  whereAmI();
 });
+
+/* console.log('Test start');
+
+setTimeout(() => console.log('o sec timer'), 0);
+Promise.resolve('Resolved Promise 1').then(res => console.log(res));
+Promise.resolve('Resolved promise 2').then(res => {
+  for (let i = 0; i < 1000000000; i++) {}
+  console.log(res);
+});
+
+console.log('Test end'); */
+//////////////////////////////////////////////////////
+
+/* const lotteryPromise = new Promise(function (resolve, reject) {
+  console.log('Lottery draw is happening');
+  setTimeout(function () {
+    if (Math.random() >= 0.5) {
+      resolve('You win!');
+    } else {
+      reject(new Error('You LOSE!'));
+    }
+  }, 2000);
+}); */
+
+// lotteryPromise.then(res => console.log(res)).catch(err => console.error(err));
+
+// Promisifying SetTimeout
+/* const wait = function (ms) {
+  return new Promise(res => {
+    setTimeout(res, ms);
+  });
+};
+
+
+wait(2000)
+  .then(() => {
+    console.log(`I've waited 2 s`);
+    return wait(1000);
+  })
+  .then(() => console.log('I waited 1 s'));
+
+
+// Promise.resolve('abc').then(res => console.log(res));
+// Promise.reject(new Error('abc')).catch(res => console.log(res));
+ */
+/* 
+///////CHALLENGE 2//////////////////
+const wait = function (ms) {
+  return new Promise(res => {
+    setTimeout(res, ms);
+  });
+};
+
+const createImage = imgPath => {
+  return new Promise((resolve, reject) => {
+    const imgEl = document.createElement('img');
+    imgEl.src = imgPath;
+
+    imgEl.addEventListener('load', () => {
+      const containerEl = document.querySelector('.images');
+      containerEl.appendChild(imgEl);
+      resolve(imgEl);
+    });
+
+    imgEl.addEventListener('error', () => {
+      reject(new Error('Error loading the file'));
+    });
+  });
+};
+let currentImg;
+createImage('img/img-1.jpg')
+  .then(img => {
+    currentImg = img;
+    return wait(2000);
+  })
+  .then(() => {
+    currentImg.style.display = 'none';
+    return createImage('img/img-2.jpg');
+  })
+  .then(img => {
+    currentImg = img;
+    return wait(2000);
+  })
+  .then(() => {
+    currentImg.style.display = 'none';
+    return createImage('img/img-3.jpg');
+  })
+  .then(img => {
+    currentImg = img;
+    return wait(2000);
+  })
+  .then(() => {
+    currentImg.style.display = 'none';
+  })
+
+  .catch(err => console.error(err)); */
+
+//////////////////////////////////////////////////////
+const whereAmI2 = async function (country) {
+  // fetch(`https://restcountries.com/v3.1/name/${country}`)
+  // .then(res=>console.log(res));
+
+  const res = await fetch(`https://restcountries.com/v3.1/name/${country}`);
+  const data = await res.json();
+  renderCountry(data[0]);
+  console.log(data[0]);
+};
+
+whereAmI2('portugal');
